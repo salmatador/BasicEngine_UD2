66eda9743a5ccf7c9458d42bab8a3feae0201633
salmatador <salmatador@moonstub.com>
2016-04-19 18:06:06 -0700

    New Commit

--- app/src/main/java/com/moonstub/basicengine/Game.java
+++ app/src/main/java/com/moonstub/basicengine/Game.java
@@ -1,14 +1,52 @@
 package com.moonstub.basicengine;
 
-import com.moonstub.basicengine.screens.LoadingScreen;
 import com.moonstub.basicengine.framework.GameActivity;
 import com.moonstub.basicengine.framework.GameScreen;
-import com.moonstub.basicengine.screens.LoadingScreen;
 
 public class Game extends GameActivity {
 
     protected GameScreen initializeScreen() {
-        return new LoadingScreen(this);
+        return new TestScreen(this);
     }
 
+    private class TestScreen extends GameScreen {
+        public TestScreen(Game game) {
+            super(game);
+        }
+
+        @Override
+        public void init() {
+
+        }
+
+        @Override
+        public void update(float delta) {
+
+        }
+
+        @Override
+        public void draw(float delta) {
+
+        }
+
+        @Override
+        public void resume() {
+
+        }
+
+        @Override
+        public void pause() {
+
+        }
+
+        @Override
+        public void dispose() {
+
+        }
+
+        @Override
+        public boolean onBackPressed() {
+            return false;
+        }
+    }
 }
--- app/src/main/java/com/moonstub/basicengine/GameAssets.java
+++ app/src/main/java/com/moonstub/basicengine/GameAssets.java
@@ -14,48 +14,6 @@ public class GameAssets {
         //Add more options for more screens Loading Screen Loads Assets
     }
 
-    public static GameImage BlueGem3H;
-    public static GameImage BlueGem3V;
-    public static GameImage GreenGem3H;
-    public static GameImage GreenGem3V;
-    public static GameImage YellowGem3H;
-    public static GameImage YellowGem3V;
-    public static GameImage RedGem3H;
-    public static GameImage RedGem3V;
-    public static GameImage WhiteGem3H;
-    public static GameImage WhiteGem3V;
-    public static GameImage OrangeGem3H;
-    public static GameImage OrangeGem3V;
-    public static GameImage PurpleGem3H;
-    public static GameImage PurpleGem3V;
-
-
-    public static GameImage BlueGem;
-    public static GameImage GreenGem;
-    public static GameImage YellowGem;
-    public static GameImage RedGem;
-    public static GameImage WhiteGem;
-    public static GameImage OrangeGem;
-    public static GameImage PurpleGem;
-    public static GameImage BombGem;
-
-    public static GameImage Blue3Bomb;
-    public static GameImage Green3Bomb;
-    public static GameImage Yellow3Bomb;
-    public static GameImage Red3Bomb;
-    public static GameImage White3Bomb;
-    public static GameImage Purple3Bomb;
-    public static GameImage Orange3Bomb;
-
-    public static GameImage GameBoard;
-    //Splash Screen Assets
-    public static GameImage SplashImage;
-    //MainMenu Assets
-    public static GameImage PlayBtnMenu;
-    public static GameImage PlayBtnMenuDefault;
-    public static GameImage PlayBtnMenuPressed;
-    public static GameImage PlayBtnMenu_Depressed;
-    public static GameImage HowToPlayBtnMenu;
-    public static GameImage HowToPlayBtnMenu_Depressed;
+    public static GameImage TestAsset;
 
 }
